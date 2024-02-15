ff75fe59fa296e88a34d743e9f3dba4a37ae57a1
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-02-15 21:41:18 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,8 +14,23 @@ class BasicAuth(Auth):
         """
         if not authorization_header or type(authorization_header) is not str:
             return None
-
         if not authorization_header.startswith('Basic '):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """the decoded value of a Base64 string base64_authorization_header"""
+        header = base64_authorization_header
+
+        if not header or type(header) is not str:
+            return None
+
+        try:
+            code = base64.b64decode(base64_authorization_header)
+            return code.decode('utf-8')
+
+        except Exception:
+            return None
