4475896261893f404185f87c55217f1be0bb2a64
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-02-17 13:48:36 +0000

    Session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,14 +13,19 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
+auth = getenv('AUTH_TYPE')
 
-if getenv('AUTH_TYPE') == 'auth':
+if auth == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-else:
+
+elif auth == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
+elif auth == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+import uuid
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """instance method that creates a Session
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID: """
+
+        if not session_id or type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
