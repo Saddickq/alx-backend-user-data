5c751ed3fc860eafd84c44cba53e456c3d643f4c
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-02-21 22:37:56 +0000

    correction for type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,11 +20,11 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str) -> User:
         """Register a user"""
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            if self._db.find_user_by(email=email):
+                raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
