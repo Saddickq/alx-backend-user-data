d31f317b97f3a91b56265c051eae87c55a8aad49
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-02-22 23:29:03 +0000

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask module
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -41,6 +41,18 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ logout route"""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user_id=user.id)
+            return redirect(url_for('messge'))
+    abort(403)
 
 
 if __name__ == "__main__":
